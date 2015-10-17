56c8022ee8bc5a3c5acfd62d5e67ef9ba6b1a8f2
YourMistake <erik.vanlandingham@myemail.indwes.edu>
2015-10-17 11:55:03 -0400

    Official Submission
    Comments and "Nicities" added for ease of use

--- Ex1_ExpressionCalculation.asm
+++ Ex1_ExpressionCalculation.asm
@@ -1,4 +1,7 @@
 ; Chapter 3, Exercise 1: Expression Calculation
+; Erik Vanlandingham
+; CIS-361
+; Last Modified 10-17-15
 
 Comment !
 Description: Using the AddTwo program from Section 3.2 as a
@@ -12,30 +15,16 @@ EBX, ECX, and EDX registers.
 .stack 4096
 ExitProcess proto,dwExitCode:dword
 
-Comment !.data This is an implementation using word variables instead of simply assigning values into each register
-varA WORD 0002h
-varB WORD 0001h
-varC WORD 0002h
-varD WORD 0003h
-
 .code
 main PROC
-mov eax, varA
-add eax, varB
-mov ebx, varC
-add ebx, varD
-sub eax, ebx
-mov varA, eax
-!
-.code
-main PROC
+    ; Move the values into the registers
 	mov eax, 2
     mov ebx, 3
     mov ecx, 1
     mov edx, 5
-    add eax, ebx
-    add ecx, edx
-    sub eax, ebx
+    add eax, ebx    ; 5
+    add ecx, edx    ; 6
+    sub eax, ecx    ; The result is FFFFFFFF
 
 	INVOKE ExitProcess,0
 main ENDP
--- Ex2_SymbolicIntegerConstants.asm
+++ Ex2_SymbolicIntegerConstants.asm
@@ -1,4 +1,7 @@
 ; Chapter 3 Exercise 2: Symbolic Integer Constants
+; Erik Vanlandingham
+; CIS-361
+; Last Modified 10-17-15
 
 Comment !
 Description: Write a program that defines symbolic constants
@@ -11,6 +14,7 @@ that uses the symbols as initializers.
 .stack 4096
 ExitProcess proto,dwExitCode:dword
 
+; Set the constants
 MONDAY = 0
 TUESDAY = 1
 WEDNESDAY = 2
@@ -21,11 +25,12 @@ SUNDAY = 6
 
 .data
 
+; Use the constants as elements in the array of days
 myDays BYTE MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 
 .code
 main PROC
-
+; Only declarations were required
 	INVOKE ExitProcess,0
 main ENDP
 END main
